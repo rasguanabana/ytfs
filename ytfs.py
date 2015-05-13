2cb0b6967aaf61b83d59f848c9ed6c78ff8873b8
Adrian Włosiak <adwlosiakh@gmail.com>
2015-05-13 18:16:47 +0200

    szkielet ytfs.py gotowy, czas na rozwinięcie funkcji

--- ytfs.py
+++ ytfs.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-### Przed lekturą kodu zaleca się zapoznianie z instrukcją użytkownika, w celu ułatwienia zrozumienia zachodzących
-##  tu procesów
+####                                                                                                                  #
+###  Przed lekturą kodu zaleca się zapoznianie z instrukcją użytkownika, w celu ułatwienia zrozumienia zachodzących  ##
+##   tu procesów.                                                                                                   ###
+#                                                                                                                  ####
 
 import os
 import stat
@@ -17,12 +19,12 @@ class fd_dict(dict):
         
         Parameters
         ----------
-        path: array-like
+        path : array-like
             Krotkowy identyfikator pliku, dla którego chcemy przydzielić deskryptor.
         
         Returns
         -------
-        descriptor: int
+        descriptor : int
             Deskryptor do pliku.
         """
 
@@ -50,7 +52,7 @@ class YTFS(Operations):
             'st_nlink': 2,
             'st_uid': os.getuid(),
             'st_gid': os.getgid(),
-            'st_size': 4096, #
+            'st_size': 4096,
             'st_atime': 0,
             'st_mtime': 0,
             'st_ctime': 0
@@ -88,12 +90,12 @@ class YTFS(Operations):
 
         Parameters
         ----------
-        path: str
+        path : str
             Ścieżka do skonwertowania. Może przybrać postać /, /katalog, /katalog/ lub /katalog/nazwa_pliku.
 
         Returns
         -------
-        tup_id: tuple
+        tup_id : tuple
             Dwuelementowy krotkowy identyfikator katalogu/pliku postaci (katalog, nazwa_pliku). Jeśli ścieżka prowadzi
             do katalogu głównego, to oba pola krotki przyjmą wartość None. Jeśli ścieżka prowadzi do katalogu
             wyszukiwania, to pole nazwa_pliku przyjmie wartość None.
@@ -105,24 +107,24 @@ class YTFS(Operations):
         """
 
         if not path or path.count('/') > 2:
-            raise ValueError #pusta ścieżka "" albo zbyt głęboka
+            raise ValueError("Bad path given") #pusta ścieżka "" albo zbyt głęboka
 
         try:
             split = path.split('/')
-        except AttributeError, TypeError:
-            raise ValueError #path nie jest stringiem
+        except (AttributeError, TypeError):
+            raise TypeError("Path has to be string") #path nie jest stringiem
 
         if split[0]:
-            raise ValueError #path nie zaczyna się od "/"
+            raise ValueError("Path needs to start with '/'") #path nie zaczyna się od "/"
         del split[0]
 
         try:
             if not split[-1]: split.pop() #podana ścieżka kończyła się ukośnikiem
         except IndexError:
-            raise ValueError #przynajmniej jeden element w split powinien na tę chwilę istnieć
+            raise ValueError("Bad path given") #przynajmniej jeden element w split powinien na tę chwilę istnieć
 
         if len(split) > 2:
-            raise ValueError #ścieżka jest zbyt długa
+            raise ValueError("Path is too deep. Max allowed level i 2") #ścieżka jest zbyt długa
 
         try:
             d = split[0]
@@ -134,6 +136,68 @@ class YTFS(Operations):
             f = None
 
         if not d and f:
-            raise ValueError #jest nazwa pliku, ale nie ma katalogu #przypał
+            raise ValueError("Bad path given") #jest nazwa pliku, ale nie ma katalogu #przypał
 
         return (d, f)
+
+    def __exists(self, path):
+
+        """Sprawdź czy plik o podanej ścieżce istnieje.
+
+        Parameters
+        ----------
+        path : str
+            Ścieżka do pliku
+
+        Returns
+        -------
+        exists : bool
+            True, jeśli plik istnieje. W przeciwnym razie False.
+
+        """
+
+        tid = self.__pathToTuple(path)
+
+        return ((not tid[0] and not tid[1]) or (tid[0] in self.searches and not tid[1]) or (tid[0] in self.searches and
+            tid[1] in self.searches[tid[0]]))
+
+    def getattr(self, path, fh=None):
+
+        """Atrybuty pliku."""
+
+        pass
+
+    def readdir(self, path, fh):
+
+        """Listowanie katalogu."""
+
+        pass
+
+    def mkdir(self, path, mode):
+
+        """Utworzenie katalogu."""
+        pass
+
+    def rmdir(self, path):
+
+        """Usunięcie katalogu."""
+
+        pass
+
+    def open(self, path, flags):
+
+        """Otwarcie pliku."""
+
+        pass
+
+    def read(self, path, length, offset, fh):
+
+        """Odczyt z pliku."""
+
+        pass
+
+    def flush(self, path, fh):
+
+        'Flush'
+
+        pass
