81674fe0af9507a202c1830ec101cf58a9f04049
Adrian Włosiak <adwlosiakh@gmail.com>
2015-07-15 18:43:16 +0200

    typo, update .gitignore

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from ytfs import __version__
 
 setup(
     name = 'ytfs',
-    varsion = ytfs.__version__,
+    version = __version__,
     description = "YouTube File System",
     long_description = "YTFS - FUSE based file system which enables you to search and play movies from YouTube as files - with tools of your choice.",
     url = "https://github.com/rasguanabana/ytfs",
